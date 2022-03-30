be44511b43e1e52c46591dec270c3049fb0282ca
hyperionwitch <78448387+hyperionwitch@users.noreply.github.com>
2022-03-30 13:32:26 -0500

    whoop I just started idk what's going on

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span>wooooords</span>
+      <span>more wooooords</span>
+    </div>
+  );
+}
+
+export default App;
