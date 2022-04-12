c3386d8a4eae7c1ae04d0c893e2d1f0d1617534d
hyperionwitch <78448387+hyperionwitch@users.noreply.github.com>
2022-04-12 11:40:34 -0500

    updated formatting and changed title

--- src/homepage.js
+++ src/homepage.js
@@ -6,41 +6,43 @@ const Homepage = () => {
                     {/* <img className="tevdrenmusicfade img-responsive" src={Tevdrenmusicfade} /> */}
                 </div>
                 <div className="col-sm-9 p-5 float-end rightcolumn">
-                    <h2>Test</h2>
-                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. 
-                    Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, 
-                    risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent 
-                    scelerisque tortor sed accumsan convallis. Lorem ipsum dolor sit amet, consectetur adipiscing 
-                    elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum 
-                    magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget 
-                    luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis. Lorem ipsum dolor 
-                    sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit 
-                    nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam 
-                    placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor 
-                    sed accumsan convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit 
-                    amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. 
-                    Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in 
-                    velit. Praesent scelerisque tortor sed accumsan convallis. Lorem ipsum dolor sit amet, consectetur 
-                    adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget 
-                    elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, 
-                    eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis. Lorem ipsum 
-                    dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis 
-                    velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam 
-                    placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor 
-                    sed accumsan convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet 
-                    pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque 
-                    vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. 
-                    Praesent scelerisque tortor sed accumsan convallis. Lorem ipsum dolor sit amet, consectetur 
-                    adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget 
-                    elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, 
-                    eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis. Lorem ipsum 
-                    dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit 
-                    nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, 
-                    purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan 
-                    convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. 
-                    Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus 
-                    eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque 
-                    tortor sed accumsan convallis.</span>
+                    <div className="bodytext">
+                        <h2>Welcome to the Test Zone<span className="superscript">TM</span></h2>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. 
+                        Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, 
+                        risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent 
+                        scelerisque tortor sed accumsan convallis. Lorem ipsum dolor sit amet, consectetur adipiscing 
+                        elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum 
+                        magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget 
+                        luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis. Lorem ipsum dolor 
+                        sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit 
+                        nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam 
+                        placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor 
+                        sed accumsan convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit 
+                        amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. 
+                        Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in 
+                        velit. Praesent scelerisque tortor sed accumsan convallis. Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget 
+                        elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, 
+                        eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis. Lorem ipsum 
+                        dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis 
+                        velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam 
+                        placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor 
+                        sed accumsan convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet 
+                        pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque 
+                        vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. 
+                        Praesent scelerisque tortor sed accumsan convallis. Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget 
+                        elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, 
+                        eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis. Lorem ipsum 
+                        dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit 
+                        nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, 
+                        purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan 
+                        convallis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. 
+                        Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus 
+                        eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque 
+                        tortor sed accumsan convallis.</span>
+                    </div>
                 </div>
             </div>
         </div>
